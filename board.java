0c0dca11c4aa855aa81e57fede6d3364c4274b3c
Shreyachepuri <91463394+Shreyachepuri@users.noreply.github.com>
2022-07-04 22:26:14 +0530

    Add files via upload

---
+++ board.java
@@ -0,0 +1,69 @@
+**
+ * 
+ */
+
+/** 
+ * @author Administrator
+ * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class board {
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object board_id;
+
+	/** 
+	 * @return the board_id
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getBoard_id() {
+		// begin-user-code
+		return board_id;
+		// end-user-code
+	}
+
+	/** 
+	 * @param theBoard_id the board_id to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setBoard_id(Object theBoard_id) {
+		// begin-user-code
+		board_id = theBoard_id;
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object board_name;
+
+	/** 
+	 * @return the board_name
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getBoard_name() {
+		// begin-user-code
+		return board_name;
+		// end-user-code
+	}
+
+	/** 
+	 * @param theBoard_name the board_name to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setBoard_name(Object theBoard_name) {
+		// begin-user-code
+		board_name = theBoard_name;
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void typr_of_board() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+}
