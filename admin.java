7392be8ce18ae3f3f9d745dbd7475261d60dcf53
Shreyachepuri <91463394+Shreyachepuri@users.noreply.github.com>
2022-07-04 22:29:04 +0530

    Add files via upload

---
+++ admin.java
@@ -0,0 +1,159 @@
+**
+ * 
+ */
+
+/** 
+ * @author Administrator
+ * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class admin {
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object admin_id;
+
+	/** 
+	 * @return the admin_id
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getAdmin_id() {
+		// begin-user-code
+		return admin_id;
+		// end-user-code
+	}
+
+	/** 
+	 * @param theAdmin_id the admin_id to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setAdmin_id(Object theAdmin_id) {
+		// begin-user-code
+		admin_id = theAdmin_id;
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object password;
+
+	/** 
+	 * @return the password
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getPassword() {
+		// begin-user-code
+		return password;
+		// end-user-code
+	}
+
+	/** 
+	 * @param thePassword the password to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setPassword(Object thePassword) {
+		// begin-user-code
+		password = thePassword;
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object usernmae;
+
+	/** 
+	 * @return the usernmae
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getUsernmae() {
+		// begin-user-code
+		return usernmae;
+		// end-user-code
+	}
+
+	/** 
+	 * @param theUsernmae the usernmae to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setUsernmae(Object theUsernmae) {
+		// begin-user-code
+		usernmae = theUsernmae;
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object email_id;
+
+	/** 
+	 * @return the email_id
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getEmail_id() {
+		// begin-user-code
+		return email_id;
+		// end-user-code
+	}
+
+	/** 
+	 * @param theEmail_id the email_id to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setEmail_id(Object theEmail_id) {
+		// begin-user-code
+		email_id = theEmail_id;
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void add_user() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void update_user() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void delete_user() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void generate_bill() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void generate_msg() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+}
