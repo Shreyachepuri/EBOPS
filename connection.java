e8973b5ffa4c08727ce7587820bdb0ed2845bb07
Shreyachepuri <91463394+Shreyachepuri@users.noreply.github.com>
2022-07-04 22:21:40 +0530

    Add files via upload

---
+++ connection.java
@@ -0,0 +1,124 @@
+/**
+ * 
+ */
+
+/** 
+ * @author Administrator
+ * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class connection {
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object connection_id;
+
+	/** 
+	 * @return the connection_id
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getConnection_id() {
+		// begin-user-code
+		return connection_id;
+		// end-user-code
+	}
+
+	/** 
+	 * @param theConnection_id the connection_id to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setConnection_id(Object theConnection_id) {
+		// begin-user-code
+		connection_id = theConnection_id;
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object connection_type;
+
+	/** 
+	 * @return the connection_type
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getConnection_type() {
+		// begin-user-code
+		return connection_type;
+		// end-user-code
+	}
+
+	/** 
+	 * @param theConnection_type the connection_type to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setConnection_type(Object theConnection_type) {
+		// begin-user-code
+		connection_type = theConnection_type;
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object connectio_name;
+
+	/** 
+	 * @return the connectio_name
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getConnectio_name() {
+		// begin-user-code
+		return connectio_name;
+		// end-user-code
+	}
+
+	/** 
+	 * @param theConnectio_name the connectio_name to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setConnectio_name(Object theConnectio_name) {
+		// begin-user-code
+		connectio_name = theConnectio_name;
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void add_connections() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void edit_connections() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void delete_connetions() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void search_connection() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+}
